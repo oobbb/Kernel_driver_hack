c55b52f41502913eeb5c6008a2fa3440e4a74a03
oobbb <89177240+oobbb@users.noreply.github.com>
2023-06-26 19:12:52 +0800

    Update entry.c
    dispatch_ioctl函数少了个大括号

--- kernel/entry.c
+++ kernel/entry.c
@@ -21,51 +21,52 @@ long dispatch_ioctl(struct file* const file, unsigned int const cmd, unsigned lo
 {
     static COPY_MEMORY cm;
     static MODULE_BASE mb;
-    static char key[0x100] = {0};
-    static char name[0x100] = {0};
+    static char key[0x100] = { 0 };
+    static char name[0x100] = { 0 };
     static bool is_verified = false;
 
-    if(cmd == OP_INIT_KEY && !is_verified) {
-        if (copy_from_user(key, (void __user*)arg, sizeof(key)-1) != 0) {
+    if (cmd == OP_INIT_KEY && !is_verified) {
+        if (copy_from_user(key, (void __user*)arg, sizeof(key) - 1) != 0) {
             return -1;
         }
-    switch (cmd) {
+        switch (cmd) {
         case OP_READ_MEM:
-            {
-                if (copy_from_user(&cm, (void __user*)arg, sizeof(cm)) != 0) {
-                    return -1;
-                }
-                if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
-                    return -1;
-                }
+        {
+            if (copy_from_user(&cm, (void __user*)arg, sizeof(cm)) != 0) {
+                return -1;
             }
-            break;
+            if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
+                return -1;
+            }
+        }
+        break;
         case OP_WRITE_MEM:
-            {
-                if (copy_from_user(&cm, (void __user*)arg, sizeof(cm)) != 0) {
-                    return -1;
-                }
-                if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
-                    return -1;
-                }
+        {
+            if (copy_from_user(&cm, (void __user*)arg, sizeof(cm)) != 0) {
+                return -1;
             }
-            break;
+            if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
+                return -1;
+            }
+        }
+        break;
         case OP_MODULE_BASE:
-            {
-                if (copy_from_user(&mb, (void __user*)arg, sizeof(mb)) != 0 
-                ||  copy_from_user(name, (void __user*)mb.name, sizeof(name)-1) !=0) {
-                    return -1;
-                }
-                mb.base = get_module_base(mb.pid, name);
-                if (copy_to_user((void __user*)arg, &mb, sizeof(mb)) !=0) {
-                    return -1;
-                }
+        {
+            if (copy_from_user(&mb, (void __user*)arg, sizeof(mb)) != 0
+                || copy_from_user(name, (void __user*)mb.name, sizeof(name) - 1) != 0) {
+                return -1;
             }
-            break;
+            mb.base = get_module_base(mb.pid, name);
+            if (copy_to_user((void __user*)arg, &mb, sizeof(mb)) != 0) {
+                return -1;
+            }
+        }
+        break;
         default:
             break;
+        }
+        return 0;
     }
-    return 0;
 }
 
 struct file_operations dispatch_functions = {
